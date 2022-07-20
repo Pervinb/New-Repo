f85034e145655d082c8ab9387e16e52213b0e9ed
parvin <bagirovp@gmail.com>
2022-07-20 15:26:14 +0400

    new commit

---
+++ dma.js
@@ -0,0 +1,19 @@
+//- eded daxil edilir, ve hemin edede qeder olan butun TƏK ededlerin cemin tapmaq
+
+let x = window.prompt('EDED DAXIL EDIN')
+
+let count = 0;
+let sum = 0;
+
+
+
+while(count < x){
+    if((count % 2) === 1){
+        sum = sum + count;
+        
+        
+    }
+    count++;
+    
+}
+console.log(sum)
